56c77c655b42b98555c577006f3324fe26557f35
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-10-20 15:36:16 +0200

    Move all styles and app skeleton to one place

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container">
+      <Head>
+        <title>Next Fao; App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="main">
+        <Component {...pageProps} />
+      </main>
+
+      <footer className="footer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
