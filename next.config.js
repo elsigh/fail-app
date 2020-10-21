08405c87af2aff2e5e0095f98ce9a2a63c222580
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-10-21 15:31:41 +0200

    Add fallback DSN and release name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   env: {
-    SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN,
-    SENTRY_RELEASE: process.env.SENTRY_RELEASE,
+    SENTRY_DSN:
+      process.env.NEXT_PUBLIC_SENTRY_DSN ||
+      process.env.SENTRY_DSN ||
+      "https://0f55dd6b18ad496686abbd5ce1847687@o142405.ingest.sentry.io/5474598",
+    SENTRY_RELEASE: process.env.SENTRY_RELEASE || "fallback-release",
   },
   experimental: { plugins: true },
 };
