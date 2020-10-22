885285344a006d1e6a54070100e374a84e9d34c1
elsigh <elsigh@gmail.com>
2020-10-21 21:00:40 -0700

    explique

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,8 @@ const Index = () => (
       <li>
         <p>
           getStaticProps calls a Promise that rejects, but does not handle
-          the rejection or await its result (returning synchronously). `revalidate: 1` is set. 
-          Sentry should record Error('Server Test 3').
+          the rejection or await its result (returning synchronously). 
+          `revalidate: 1` is set. Sentry should record Error('Server Test 4').
         </p>
         <a href="/server/test4" target="_blank">
           Open in a new tab
