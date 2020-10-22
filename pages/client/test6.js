3d3a3e9786a486c7b8dabc94ba3b4e333df24dde
elsigh <elsigh@gmail.com>
2020-10-22 14:34:54 -0700

    update 6

--- pages/client/test6.js
+++ pages/client/test6.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default class Test6 extends React.Component {
   render() {
-    throw new Error("Client Test 6")
+    throw new Error("Client Test")
     return (
       <div>
         <h1>Client Test 6</h1>
