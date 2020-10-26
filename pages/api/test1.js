26d70130937352573e3a232613b590a501f6cfb2
elsigh <elsigh@gmail.com>
2020-10-25 20:01:53 -0700

    rm api dir

--- pages/api/test1.js
+++
@@ -1,7 +0,0 @@
-const doAsyncWork = () => Promise.reject(new Error('API Test 1'))
-doAsyncWork()
-
-export default function handler(req, res) {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
